4d41f351c0a905474bc57473a3ee4d95d9be9cc6
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-04-30 23:20:22 -0300

    [test]:[test feature 2] test if filters is correctly applied

--- tests/tst_feat2.py
+++ tests/tst_feat2.py
@@ -1,50 +1,51 @@
 '''
 feature 2 - filter games:
 
-the test chooses at random 1 game for each required field (plataforma,  ano de lançamento, categoria, 
-desenvolvedora) then filter the game for each field and chooses 10 games at random. If a single filtered
+the test chooses at random 1 game for one required field (plataforma,  ano de lançamento, categoria, 
+desenvolvedora) then filter the game list for each field and chooses 10 games at random. If a single filtered
 game dont have the same selected field the  test fails. Otherwise the test passes
 
 '''
 
-
-import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import pytest
+import random
 
-
-def test1():
-    for i in range(0,2):
-        driver = webdriver.Chrome()
-        driver.get("http://localhost:4200/")
-        time.sleep(5)
-        gameButtons = driver.find_elements(By.CLASS_NAME, "gameButton")
-        print(len(gameButtons))
-        assert len(gameButtons) > 10
+def test():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:4200/")
     
-        gameButton = gameButtons[random.randint(0,len(gameButtons)-1)]
-        gameButton.click()
-        time.sleep(3)
-        print("current url " + driver.current_url)
-        params = []
-        params.append(driver.find_element(By.CLASS_NAME, "gameTitle").text)
-        params.append(driver.find_element(By.CLASS_NAME, "gameDescription").text)
-        params.append(driver.find_element(By.CLASS_NAME, "gamePublisher").text)
-        params.append(driver.find_element(By.CLASS_NAME, "gameRealease").text)
-        params.append(driver.find_element(By.CLASS_NAME, "gamePlatform").text)
-        params.append(driver.find_element(By.CLASS_NAME, "gamesc").get_attribute("src"))
-        
-        for param in params:
-            print(param)
-            assert param != None
-            assert len(param) > 1
-        
-        driver.back()
-        time.sleep(5)
-        driver.quit()
+    time.sleep(4)
+    gameButtons = driver.find_elements(By.CLASS_NAME, "gameButton")
+    time.sleep(4)
+    
+    gameButton = gameButtons[random.randint(0,len(gameButtons)-1)]
+    platform = gameButton.find_element(By.CLASS_NAME, "gamePlatform").text
+    
+    print(platform)
 
+    filterPanel = driver.find_element(By.CLASS_NAME, "filterPanel")
+    inputs = filterPanel.find_elements(By.TAG_NAME, "input")
+    filterButton = filterPanel.find_element(By.CLASS_NAME,"filterButton")
+
+    myinput=None
+
+    for input in inputs:
+        if input.get_attribute("placeholder")=="platform...":
+            myinput = input
+    myinput.send_keys(platform)
+    time.sleep(0.5)
+    filterButton.click()
+    time.sleep(5)
+    gameButtons = driver.find_elements(By.CLASS_NAME, "gameButton")
+
+    for button in gameButtons:
+        assert button.find_element(By.CLASS_NAME,"gamePlatform").text == platform
+
+    driver.quit()
     pass
 
-test1()
+
+test()
---
+++ tests/tst_feat3.py
@@ -0,0 +1,43 @@
+'''
+feature 3 - game detail:
+
+the test chooses a game at random from the game list and after a event click
+must assert that it goes to a new page detail and all required fields are valid.
+
+'''
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pytest
+
+
+def test1():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:4200/")
+    time.sleep(5)
+    gameButtons = driver.find_elements(By.CLASS_NAME, "gameButton")
+    print(len(gameButtons))
+    assert len(gameButtons) > 10
+
+    gameButton = gameButtons[random.randint(0,len(gameButtons)-1)]
+    gameButton.click()
+    time.sleep(3)
+    print("current url " + driver.current_url)
+    params = []
+    params.append(driver.find_element(By.CLASS_NAME, "gameTitle").text)
+    params.append(driver.find_element(By.CLASS_NAME, "gameDescription").text)
+    params.append(driver.find_element(By.CLASS_NAME, "gamePublisher").text)
+    params.append(driver.find_element(By.CLASS_NAME, "gameRealease").text)
+    params.append(driver.find_element(By.CLASS_NAME, "gamePlatform").text)
+    params.append(driver.find_element(By.CLASS_NAME, "gamesc").get_attribute("src"))
+    
+    for param in params:
+        print(param)
+        assert param != None
+        assert len(param) > 1
+    
+    driver.quit()
+    pass
+
+test1()
